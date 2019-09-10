6b3df5ce8c543e0a82e45a5ace8699e36b6a9acc
gianc <giancarlogavotti@gmail.com>
2019-09-10 11:36:58 +0200

    prime numbers

--- ex1.py
+++ ex1.py
@@ -1,2 +1,8 @@
-print("Hello World")
-print("hi")
+x= list(range(2, 100+1))
+for i in x:
+    j=2
+    while(i* j <=x[-1]):
+        if(i * j in x):
+            x.remove(i * j)
+        j += 1
+print("the list of prime numbers is", x)
